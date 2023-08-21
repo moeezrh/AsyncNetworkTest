0578c65698b3b096da8315ee7716be62637230a1
Moeez <moeez123104@gmail.com>
2023-08-21 07:46:28 -0700

    Summary and Event File for Asynchronous Test

--- asyncping.py
+++ asyncping.py
@@ -8,41 +8,8 @@ import sys
 from datetime import datetime
 import shutil
 
-logging.basicConfig(level=logging.INFO)
 
-# Set the duration for the loop in seconds
-duration = 60*60  # Change this to the desired duration in seconds
-
-# temp directory path
-if getattr(sys, 'frozen', False):
-    application_path = os.path.dirname(sys.executable)
-elif __file__:
-    application_path = os.path.dirname(__file__)
-
-temp_path = os.path.join(application_path, "temp")
-
-for item in os.listdir(temp_path):
-    item_path = os.path.join(temp_path, item)
-    if os.path.isfile(item_path):  # Check if it's a file
-        os.remove(item_path)  # Remove the file
-    elif os.path.isdir(item_path):  # Check if it's a directory
-        shutil.rmtree(item_path)  # Remove the directory and its contents
-
-# Date and time of when Ping Testing Begins
-now = datetime.now()
-dt_print = now.strftime("%m/%d/%Y at %I:%M:%S %p")
-start_time = now.strftime("%I:%M:%S %p")
-
-print("Scanning network for devices")
-ip_addr = "192.168.1.1/23"
-scanned_output = scan(ip_addr)
-print("Testing the following IP Addresses " + dt_print + "\n")  
-ip_addresses = ip_results(scanned_output)
-print("Number of Devices:",len(ip_addresses))
-
-hosts = ip_addresses
-
-async def do_ping(host):
+async def do_ping(host, duration):
 
     if getattr(sys, 'frozen', False):
         application_path = os.path.dirname(sys.executable)
@@ -65,6 +32,13 @@ async def do_ping(host):
 
         print("\nPinging...")
 
+        total_offline = 0
+        total_online = 0
+        uptime = 0
+        result = 0
+
+        file.write("EVENT LOG:\n")
+
         while ((time.time() - program_start_time) < duration) and retry_count < max_retries:
             try:           
                 delay = await aioping.ping(host, 0.5) * 1000
@@ -72,8 +46,9 @@ async def do_ping(host):
                 if state_before != state_after:
                     state_before = state_after
                     end_time = time.time()
-                    on_time = end_time - start_time
-                    file.write("\nOFFLINE for " + str(on_time) + " s")
+                    off_time = end_time - start_time
+                    total_offline += off_time
+                    file.write("\nOFFLINE for " + str(off_time) + " s")
                     start_time = end_time
 
             except TimeoutError:
@@ -84,23 +59,142 @@ async def do_ping(host):
                     state_before = state_after
                     end_time = time.time()
                     on_time = end_time - start_time
+                    total_online += on_time
                     file.write("\nONLINE for " + str(on_time) + " s")
                     start_time = end_time
                     
         else:
             if state_after == "OFFLINE":
                 end_time = time.time()
-                on_time = end_time - start_time
+                off_time = end_time - start_time
+                total_offline += off_time
                 file.write("\nOFFLINE for " + str(on_time) + " s DISCONNECTED DUE TO TIMEOUT")
             elif state_before == state_after:
                 print("Ping Test Done")
                 end_time = time.time()
                 on_time = end_time - start_time
+                total_online += on_time
                 file.write("\nONLINE for " + str(on_time) + " s")
+        
+        file.write("\nEVENT LOG END")
+
+        if total_online == 0:
+            file.write("UPTIME: N/A")
+        else:
+            uptime = float(((total_online) / (total_online + total_offline)) * 100)
+            uptime = round(uptime, 2)
+            if uptime == float(100):
+                result = "PASS"
+            else:
+                result = "FAIL"
+            file.write("\nSUMMARY: \n" + host + "\t\t" + str(uptime) + "\t\t" + result + "\nSUMMARY END")    
+        
+
+
+        
+        
+        
+
 
 async def main():
-    tasks = [do_ping(host) for host in hosts]
+
+    logging.basicConfig(level=logging.INFO)
+
+    # temp directory path (to delete all files in it)
+    if getattr(sys, 'frozen', False):
+        application_path = os.path.dirname(sys.executable)
+    elif __file__:
+        application_path = os.path.dirname(__file__)
+
+    temp_path = os.path.join(application_path, "temp")
+
+    for item in os.listdir(temp_path):
+        item_path = os.path.join(temp_path, item)
+        if os.path.isfile(item_path):  # Check if it's a file
+            os.remove(item_path)  # Remove the file
+        elif os.path.isdir(item_path):  # Check if it's a directory
+            shutil.rmtree(item_path)  # Remove the directory and its contents
+
+    # Initial Start Date and Time of Program
+    now = datetime.now()
+    dt_string = now.strftime("%m_%d_%Yat%I_%M_%S%p")
+    dt_print = now.strftime("%m/%d/%Y at %I:%M:%S%p")
+
+    if getattr(sys, 'frozen', False):
+        application_path = os.path.dirname(sys.executable)
+    elif __file__:
+        application_path = os.path.dirname(__file__)
+ 
+    output_folder = 'Outputs\\'
+
+    # Path for output file
+    output_path = os.path.join(application_path, output_folder)   
+
+    # Saving the path for output, summary, and event file
+    outputSummaryFile = output_path
+    outputEventFile = output_path
+
+    outputSummaryFile += dt_string + "_Summary.txt"
+    outputEventFile += dt_string + "_Event_Log.txt"
+
+    # User inputs testing time length
+    hours = 8
+    print("How many hours would you like to test for?")
+    test_hours = float(input("Type in the correct hours: "))
+    if (test_hours != ""):
+         hours = test_hours
+    print(hours)
+    seconds = float(3600 * hours)
+    duration = seconds
+
+    # Date and time of when Ping Testing Begins
+    now = datetime.now()
+    dt_print = now.strftime("%m/%d/%Y at %I:%M:%S %p")
+    start_time = now.strftime("%I:%M:%S %p")
+
+    # Scanning network for devices
+    print("Scanning network for devices")
+    ip_addr = "192.168.1.1/23"
+    scanned_output = scan(ip_addr)
+    print("Testing the following IP Addresses " + dt_print + "\n")  
+    ip_addresses = ip_results(scanned_output)
+    print("Number of Devices:",len(ip_addresses))
+
+    hosts = ip_addresses
+
+    tasks = [do_ping(host, duration) for host in hosts]
     await asyncio.gather(*tasks)
+
+
+    # Summary File Header
+    with open(outputSummaryFile, "a") as file:
+        file.write("Summary Results\n\n" + str(hours) + " Hour Test\n\nSTART TIME: " + start_time + "\n\n")
+
+    # Event Log Header
+    with open(outputEventFile, "a") as file:
+         file.write("Event Log \n\n" + str(hours) + " Hour Test\n\nSTART TIME: " + start_time + "\n") 
+
+
+    for each_file in os.listdir(temp_path):
+        file_ip = each_file
+        file_ip_start = 0
+        file_ip_end = file_ip.find(".txt")
+        file_ip = file_ip[file_ip_start: file_ip_end]
+        each_file = os.path.join(temp_path, each_file)
+        with open(each_file, "r") as source_file, open(outputEventFile, "a") as destination_file:
+            output = source_file.read()
+            event_file_start = output.find("EVENT LOG:")
+            event_file_end = output.find("EVENT LOG END")
+            event_file = output[event_file_start + 10: event_file_end]
+            destination_file.write("\n" + file_ip + ":" + event_file + "\n")
+        with open(each_file, "r") as source_file, open(outputSummaryFile, "a") as destination_file:
+            output = source_file.read()
+            summary_file_start = output.find("SUMMARY")
+            summary_file_end = output.find("SUMMARY END")
+            summary_file = output[summary_file_start + 10: summary_file_end]
+            destination_file.write(summary_file)
+
+
 
 if __name__ == "__main__":
     asyncio.run(main())
